d9acffd09e2448dbc86e097ebb8f19673bec9737
ksw4015 <zealot4321@naver.com>
2024-07-03 20:42:54 +0900

    feature: Main Home 구현
    1. 검색창, 카테고리, 목록 구현

--- wabiz_client/lib/view/home/home_page.dart
+++ wabiz_client/lib/view/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:wabiz_client/theme.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,8 +13,189 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+      backgroundColor: WabizColors.scaffoldBackgroundColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.white,
+              height: 324,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: SizedBox(
+                            height: 40,
+                            child: TextFormField(
+                              onTap: () {
+
+                              },
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                  borderSide: BorderSide(
+                                    color: WabizColors.wabizGray[100]!
+                                  )
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                    borderSide: BorderSide(
+                                      color: WabizColors.wabizGray[100]!,
+                                    ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                  borderSide: const BorderSide(
+                                    color: WabizColors.primary,
+                                  ),
+                                ),
+                                hintText: '새로운 일상이 필요하신가요?',
+                                suffixIcon: const Icon(
+                                  Icons.search,
+                                ),
+                                suffixIconColor: WabizColors.wabizGray[400]!
+                              ),
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+
+                          },
+                          icon: Icon(
+                            Icons.notifications_none
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView.builder(
+                      scrollDirection: Axis.horizontal,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 12,
+                        mainAxisSpacing: 0
+                      ),
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          child: Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 38,
+                                backgroundColor: WabizColors.background,
+                              ),
+                              Gap(4),
+                              Text('펀딩+')
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: 48,
+                    height: 4,
+                    decoration: BoxDecoration(
+                      color: WabizColors.background,
+                      borderRadius: BorderRadius.circular(2)
+                    ),
+                  ),
+                  Gap(12)
+                ],
+              ),
+            ),
+            Gap(12),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    child: Container(
+                      margin: const EdgeInsets.only(bottom: 8, left: 16, right: 16, top: 20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: const Offset(0, 8),
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 30,
+                            spreadRadius: 4
+                          )
+                        ]
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 220,
+                            decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                topRight: Radius.circular(10)
+                              )
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '12,000명이 기다려요!',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w700,
+                                    color: WabizColors.primary
+                                  ),
+                                ),
+                                Gap(8),
+                                Text(
+                                  '아이돌 관리비법 | 준비안된 얼굴라인도 살리는 세럼',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                Gap(16),
+                                Text(
+                                  '세상에 없던 브랜드',
+                                  style: TextStyle(
+                                      color: WabizColors.wabizGray[500]
+                                  ),
+                                ),
+                                Gap(16),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: WabizColors.background,
+                                    borderRadius: BorderRadius.circular(3)
+                                  ),
+                                  padding: EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+                                  child: Text(
+                                    '오픈예정',
+                                    style: TextStyle(
+                                      fontSize: 10
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
